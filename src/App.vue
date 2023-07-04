9fcb0cf99a881b7e788ff3a2b425e935d93fa798
superyngo <superyngo@gmail.com>
2023-07-05 05:12:33 +0800

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+const state = [
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+  {tag: "li", title: "Travel Maker", link: "#"},
+];
+</script>
+
+<template>
+  <div class="navbar">
+    <ul class="navbar-nav">
+      <component
+        v-for="comp of state"
+        :is="comp.tag"
+        class="item"
+        key="comp.title"
+      >
+        <a href="comp.link">{{ comp.title }} </a>
+      </component>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  position: relative;
+  .navbar-nav {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    justify-content: center;
+
+    .item {
+      list-style: none;
+    }
+  }
+}
+</style>
